8f4577d1fa2a11f81723485c86845bcc2b6ccf11
Menno Spijker <spijkermenno@gmail.com>
2026-04-15 18:31:36 +0200

    Project setup from AndroidStudio

---
+++ app/src/main/java/nl/menosa/cleverbaseAssesment/MainActivity.kt
@@ -0,0 +1,47 @@
+package nl.menosa.cleverbaseAssesment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nl.menosa.cleverbaseAssesment.ui.theme.CleverBaseAssesmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleverBaseAssesmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CleverBaseAssesmentTheme {
+        Greeting("Android")
+    }
+}
